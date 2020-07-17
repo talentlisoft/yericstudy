8b54b0e697d168c4036794560d4397241be50e6c
Lisoft <talentlisoft@gmail.com>
2020-07-17 19:01:11 +0800

    something

--- app/Http/Controllers/Api/apitopicsController.php
+++ app/Http/Controllers/Api/apitopicsController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\Admin\topicsBaseController;
 
 class apitopicsController extends topicsBaseController
